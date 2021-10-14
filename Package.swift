3e28e9a8ac8704e78cde6557f9042ffb490cb7b9
ILYA SHKOLNIK <ilya2606@mail.ru>
2021-10-14 18:56:14 +0300

    1.0.1

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         .target(
             name: "AppleDeviceModelPlist",
             dependencies: [],
-            resources: [.copy("Resources")]
+            resources: [.process("Resources")]
         ),
     ]
 )
